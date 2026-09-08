aa01d024b0196eaf95309034f5c1290893746d27
Chuck Cassel <619504+casselc@users.noreply.github.com>
2026-09-08 03:22:24 -0700

    feat(plotje): calculate aggregate series

--- src/oscope/query/expression.clj
+++ src/oscope/query/expression.clj
@@ -5,9 +5,11 @@
 
 (def percentile-presets #{50 75 90 95 99})
 (def aggregate-ops #{:count :sum :avg :min :max :percentile})
+(def calculation-ops #{:add :subtract :multiply :divide})
 (def max-groups 2)
 (def max-filters 4)
 (def max-series 8)
+(def max-calculations 6)
 (def max-group-text-length 160)
 (def max-source-rows 100000)
 (def max-source-bytes 67108864)
@@ -48,9 +50,10 @@
     :numbers {:value "Value"}}})
 
 (def ^:private expression-keys
-  #{:signal :window :group-by :filters :series :limit})
+  #{:signal :window :group-by :filters :series :calculations :limit})
 (def ^:private filter-keys #{:field :op :value})
 (def ^:private series-keys #{:as :op :field :percentile})
+(def ^:private calculation-keys #{:as :op :args})
 
 (defn- fail! [type message data]
   (throw (ex-info message (assoc data :oscope.query-expression/error true
@@ -71,6 +74,55 @@
   (or (get signals signal)
       (fail! ::unsupported-signal "telemetry query signal is unsupported"
              {:signal signal :supported-signals (vec (keys signals))})))
+
+(defn- finite-number? [value]
+  (and (number? value)
+       (let [number (double value)]
+         (and (= number number) (not= number ##Inf) (not= number ##-Inf)))))
+
+(defn- validate-calculations [value series-aliases occupied-aliases]
+  (let [calculations (get value :calculations [])]
+    (when-not (and (vector? calculations)
+                   (<= (count calculations) max-calculations))
+      (fail! ::invalid-calculations
+             "calculations must contain up to 6 entries"
+             {:calculations calculations :maximum max-calculations}))
+    (let [available (set series-aliases)
+          calculations
+          (mapv
+           (fn [calculation]
+             (when-not (map? calculation)
+               (fail! ::invalid-calculation "every calculation must be a map"
+                      {:calculation calculation}))
+             (unknown! "telemetry calculation" calculation-keys calculation)
+             (let [{alias :as op :op args :args} calculation]
+               (alias! alias)
+               (when-not (contains? calculation-ops op)
+                 (fail! ::unsupported-calculation
+                        "calculation op must be :add, :subtract, :multiply, or :divide"
+                        {:op op :supported-ops (vec (sort calculation-ops))}))
+               (when-not (and (vector? args) (= 2 (count args)))
+                 (fail! ::invalid-calculation-args
+                        "calculation args must contain exactly two operands"
+                        {:args args}))
+               (doseq [operand args]
+                 (when-not (or (and (keyword? operand)
+                                    (contains? available operand))
+                               (finite-number? operand))
+                   (fail! ::invalid-calculation-operand
+                          "calculation operands must be aggregate series aliases or finite numbers"
+                          {:operand operand :series-aliases series-aliases})))
+               {:as alias :op op :args args}))
+           calculations)
+          aliases (mapv :as calculations)]
+      (when-not (= (count aliases) (count (distinct aliases)))
+        (fail! ::duplicate-calculation-alias
+               "calculation aliases must be unique" {:aliases aliases}))
+      (when-let [collision (some occupied-aliases aliases)]
+        (fail! ::alias-collision
+               "calculation aliases must not replace group-by fields or aggregate series"
+               {:alias collision :occupied-aliases (vec occupied-aliases)}))
+      calculations)))
 
 (defn validate-expression [value]
   (when-not (map? value)
@@ -155,11 +207,13 @@
         (when (some (set group-by) aliases)
           (fail! ::alias-collision "series aliases must not replace group-by fields"
                  {:group-by group-by :aliases aliases}))
-        (when-not (and (integer? limit) (<= 1 limit query/max-result-limit))
-          (fail! ::invalid-limit "telemetry query limit is outside the explorer cap"
-                 {:limit limit :minimum 1 :maximum query/max-result-limit}))
-        {:signal signal :window window :group-by group-by :filters filters
-         :series series :limit limit}))))
+        (let [calculations (validate-calculations
+                            value aliases (set (concat group-by aliases)))]
+          (when-not (and (integer? limit) (<= 1 limit query/max-result-limit))
+            (fail! ::invalid-limit "telemetry query limit is outside the explorer cap"
+                   {:limit limit :minimum 1 :maximum query/max-result-limit}))
+          {:signal signal :window window :group-by group-by :filters filters
+           :series series :calculations calculations :limit limit})))))
 
 (defn from-selection [{:keys [signal field window limit]}]
   (validate-expression
@@ -167,8 +221,42 @@
     :series [{:as :count :op :count}] :limit limit}))
 
 (defn output-fields [expression]
-  (let [{:keys [group-by series]} (validate-expression expression)]
-    (vec (concat group-by (map :as series)))))
+  (let [{:keys [group-by series calculations]} (validate-expression expression)]
+    (vec (concat group-by (map :as series) (map :as calculations)))))
+
+(defn- operand-value [row operand]
+  (if (keyword? operand)
+    (do
+      (when-not (contains? row operand)
+        (fail! ::missing-series-result
+               "calculation aggregate result is missing"
+               {:operand operand :available-fields (vec (keys row))}))
+      (let [value (get row operand)]
+        (when-not (or (nil? value) (finite-number? value))
+          (fail! ::non-numeric-series-result
+                 "calculation aggregate result must be a finite number or nil"
+                 {:operand operand :value value}))
+        value))
+    operand))
+
+(defn- calculate [{:keys [op args]} row]
+  (let [left (operand-value row (first args))
+        right (operand-value row (second args))]
+    (when (and (some? left) (some? right)
+               (not (and (= :divide op) (zero? (double right)))))
+      (let [left (double left)
+            right (double right)
+            result (case op
+                     :add (+ left right)
+                     :subtract (- left right)
+                     :multiply (* left right)
+                     :divide (/ left right))]
+        (when (finite-number? result) result)))))
+
+(defn apply-calculations [row calculations]
+  (reduce (fn [result calculation]
+            (assoc result (:as calculation) (calculate calculation row)))
+          row calculations))
 
 (defn- aggregate-sql [config {:keys [op field percentile]}]
   (let [number (get-in config [:numbers field])]
@@ -184,7 +272,8 @@
   (when-not (and (integer? now-unix-nano) (<= 1 now-unix-nano 9223372036854775807))
     (fail! ::invalid-time "telemetry query end must be epoch nanoseconds"
            {:end-unix-nano now-unix-nano}))
-  (let [{:keys [signal window group-by filters series limit] :as expression}
+  (let [{:keys [signal window group-by filters series calculations limit]
+         :as expression}
         (validate-expression expression)
         config (config! signal)
         start (max 0 (- now-unix-nano (get query/windows window)))
@@ -223,4 +312,5 @@
     {:expression expression :sqlvec (into [sql] params)
      :columns (vec (concat (map-indexed (fn [i field] [(keyword (str "dimension" i)) field]) group-by)
                            (map-indexed (fn [i item] [(keyword (str "series" i)) (:as item)]) series)))
+     :calculations calculations
      :limit limit}))
